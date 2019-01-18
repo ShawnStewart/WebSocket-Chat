8764065367160fd7d8f6a544327871f70c989491
Shawn Stewart <s.stewart0220@gmail.com>
2019-01-18 11:33:46 -0500

    Basic chat room complete

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 let app = require("express")();
 let http = require("http").Server(app);
 let path = require("path");
+let io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(path.normalize(__dirname + "/index.html"));
+});
+
+io.on("connection", socket => {
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected");
+  });
 });
 
 http.listen(3000, () => {
